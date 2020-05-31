cc7cef76d68ae21ddc3eb72ba5ff77e7490af2a8
clemens <clemensvegans@gmail.com>
2020-05-31 02:05:50 +0000

    may 31

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+/*const cookieParser = require('cookie-parser');*/
 const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
